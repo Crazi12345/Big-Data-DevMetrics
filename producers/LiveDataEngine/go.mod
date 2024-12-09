4d59686a5d47d598a006fc76f1713bd3f8da56fb
Patrick <patrickbjerre@outlook.dk>
2024-12-09 14:07:40 +0100

    added some avro conversion that works

--- producers/LiveDataEngine/go.mod
+++ producers/LiveDataEngine/go.mod
@@ -1,3 +1,14 @@
 module ingestion/avro
 
 go 1.22.9
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hamba/avro/v2 v2.27.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/linkedin/goavro/v2 v2.13.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
--- producers/LiveDataEngine/main.go
+++ producers/LiveDataEngine/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"bufio"
+	"encoding/xml"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"github.com/fatih/structs"
+	"github.com/linkedin/goavro/v2"
 )
+
+type Post struct {
+	Id                    int    `xml:"Id,attr" avro:"Id"`
+	PostTypeId            int    `xml:"PostTypeId,attr" avro:"post_type_id"`
+	ParentId              int    `xml:"ParentId,attr" avro:"parent_id"`
+	CreationDate          string `xml:"CreationDate,attr" avro:"creation_date"`
+	Score                 int    `xml:"Score,attr" avro:"score"`
+	OwnerUserId           int    `xml:"OwnerUserId,attr" avro:"owner_user_id"`
+	LastEditorUserId      int    `xml:"LastEditorUserId,attr" avro:"last_editor_user_id"`
+	LastEditorDisplayName string `xml:"LastEditorDisplayName,attr" avro:"last_editor_display_name"`
+	LastEditDate          string `xml:"LastEditDate,attr" avro:"last_edit_date"`
+	LastActivityDate      string `xml:"LastActivityDate,attr" avro:"last_activity_date"`
+	CommentCount          int    `xml:"CommentCount,attr" avro:"comment_count"`
+	CommunityOwnedDate    string `xml:"CommunityOwnedDate,attr" avro:"community_owned_date"`
+	ContentLicense        string `xml:"ContentLicense,attr" avro:"content_license"`
+}
 
 func check(e error, message string) {
 	if e != nil {
-		log.Fatal(message)
+		log.Fatal(message, e)
 	}
 }
 
+func saveToFile(filename string, data []byte) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Then write the Avro data
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 func popLine(f *os.File) ([]byte, error) {
 	// Use tail to get the first line from the file
 	cmd := exec.Command("tail", "-n", "1", f.Name())
@@ -56,4 +91,36 @@ func popLine(f *os.File) ([]byte, error) {
 
 func main() {
 
+	file, err := os.OpenFile("/run/media/tired_atlas/Maxtor/test.xml", os.O_CREATE, 0644)
+	check(err, "Could not open file")
+	reader := bufio.NewReader(file)
+	avroSchema, err := os.ReadFile("/home/tired_atlas/Projects/Big-Data-DevMetrics/producers/LiveDataEngine/postSchema.avsc")
+	check(err, "unable to parse schema") // Handle the error properly
+
+	reader.ReadLine()
+	reader.ReadLine()
+	line, _, _ := reader.ReadLine()
+	data := &Post{}
+	error := xml.Unmarshal(line, data)
+	if nil != error {
+		fmt.Println("Error unmarshalling from XML", err)
+		return
+	}
+    ocfFile, err := os.Create("pp.avro")
+	check(err, "cannot create Avro file")
+	defer ocfFile.Close()
+
+	writer, err := goavro.NewOCFWriter(goavro.OCFConfig{
+		W:      ocfFile,
+		Schema: string(avroSchema), // Use string(avroSchema) here
+	})
+	check(err, "cannot create OCF writer")
+
+	native := structs.Map(data)
+	check(err, "cannot convert to map")
+
+	// Append the data to the OCF writer
+	err = writer.Append([]map[string]interface{}{native})
+	check(err, "cannot append data to OCF writer")
+	// You can write this to a file, send it over a network, etc.
 }
