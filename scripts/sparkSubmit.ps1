28809a57cdc0b380c1ade46eaec8f4f9790fe496
Katrine Birk <katrinebirk2@live.dk>
2024-12-16 16:48:48 +0100

    spark submit fix

--- scripts/sparkSubmit.ps1
+++ scripts/sparkSubmit.ps1
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spark-submit --master spark://spark-master-svc:7077 ../services/spark/process-streaming.py
+spark-submit --master spark://spark-master-svc:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2 --conf spark.jars.ivy=/tmp/.ivy2 ../services/spark/process-streaming.py
