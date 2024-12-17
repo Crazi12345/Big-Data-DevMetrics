9b0e04264e9c6918fb417963246c71f5ca33ddff
CamillaFrank <75301585+CamillaFrank@users.noreply.github.com>
2024-12-17 14:19:17 +0100

    basic hive

---
+++ scripts/setupHive.ps1
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Set up PostgreSQL for metastore
+helm install hive-postgresql \
+  --version=12.1.5 \
+  --set auth.username=root \
+  --set auth.password=pwd1234 \
+  --set auth.database=hive \
+  --set primary.extendedConfiguration="password_encryption=md5" \
+  --repo https://charts.bitnami.com/bitnami \
+  postgresql
+
+#Apply yaml files
+kubectl apply -f hive-metastore.yaml
+kubectl apply -f hive.yaml
