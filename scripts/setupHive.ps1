005199b7fe6df06fe5b6617dfc61be20c8f678a3
Katrine Birk <katrinebirk2@live.dk>
2024-12-19 13:13:00 +0100

    hive setup database

--- scripts/setupHive.ps1
+++ scripts/setupHive.ps1
@@ -16,3 +16,36 @@ kubectl apply -f ../services/hive/hive.yaml
 
 #Portforward the UI
 # kubectl port-forward svc/hiveserver2 10002:10002
+
+#Connect to database using tool such as Data grip
+#Run the following: 
+<# drop table users;
+CREATE EXTERNAL TABLE users (
+    hirable BOOLEAN,
+    public_repos BIGINT,
+    is_suspicious BOOLEAN,
+    updated_at STRING,
+    id BIGINT,
+    blog STRING,
+    followers BIGINT,
+    location STRING,
+    follower_list int,
+    type STRING,
+    commit_list int,
+    bio STRING,
+    commits BIGINT,
+    company STRING,
+    following_list ARRAY<STRING>,
+    public_gists BIGINT,
+    name STRING,
+    created_at STRING,
+    email STRING,
+    followings BIGINT,
+    login STRING,
+    repo_list int
+)
+    row format delimited
+    fields terminated by ','
+    location 'hdfs://namenode:9000/user';
+
+#>
